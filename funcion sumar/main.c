c9e94d8366c8bebc984c99771f929a44d3a970d9
MatiasCoiman <36193664+MatiasCoiman@users.noreply.github.com>
2018-03-26 12:30:26 -0300

    funcion sumar.

---
+++ funcion sumar/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "baus.h"
+int sumar(int , int );
+int main()
+{
+    int rta;
+    int x=5;
+    int y=7;
+
+    rta=sumar(x,y);
+    printf("La suma es: %d\n ",rta);
+
+
+
+    return 0;
+}
+
+
